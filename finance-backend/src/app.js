bcd1fb9f61dbcfe84f4cb0613e494e4dbe6ad64e
rudradas05 <dasrudra.stu@gmail.com>
2026-04-04 19:45:15 +0530

    feat: add express-rate-limit

--- finance-backend/src/app.js
+++ finance-backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
@@ -11,11 +12,28 @@ dotenv.config();
 
 const app = express();
 
+
+const generalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100,
+  message: { message: "Too many requests, please try again after 15 minutes." },
+});
+
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 10, 
+  message: {
+    message: "Too many auth attempts, please try again after 15 minutes.",
+  },
+});
+
 app.use(cors());
 app.use(express.json());
+app.use(generalLimiter); 
 
 // Routes
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authLimiter, authRoutes); 
 app.use("/api/users", userRoutes);
 app.use("/api/records", recordRoutes);
 app.use("/api/dashboard", dashboardRoutes);
